4bc17eaf722185a01740328ec203329984d018c4
melquisedeque_sodre <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2022-12-29 15:41:17 -0300

    config fonts and assets

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+
+  const [isFontsLoaded] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar 
+        style='light'
+        backgroundColor='transparent'
+        translucent
+      />
+      {isFontsLoaded ? <Text>Hello World!</Text> : <Text>Carregando...</Text>}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@typings': './src/typings',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes'
+          }
+        },
+      ],
+    ],
   };
 };
